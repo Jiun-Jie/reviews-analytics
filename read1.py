a06a32cc289399c4fc885534ff3a8b24448a132a
JJ Chiou <jiunjiechiou@hotmail.com>
2019-12-05 00:03:02 +0800

    calc avg reviews

--- read1.py
+++ read1.py
@@ -6,7 +6,10 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:	# count 與1000 餘數 =0
 			print(len(data))
-print(len(data))
-print(data[0])
-print('-----------------------')
-print(data[1])
+print('檔案讀取完了, 總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+a = sum_len / len(data)
+print('每句留言長度為', a)
